323e2d48d432221210ba7b226ca288f63558734a
Michael Levan <mlevan1992@gmail.com>
2020-06-16 16:46:49 -0400

    Returns a list of vms

--- AzureCLI.py
+++ AzureCLI.py
@@ -1,13 +1,20 @@
-import os
+import azure.mgmt.compute as Compute
+from azure.common.client_factory import get_client_from_cli_profile
 import logging
+import json
 
 
 class AzureCLI:
 
     def listVMs():
         try:
-            az = os.system('az vm list')
-            return az
+            VM = get_client_from_cli_profile(Compute.ComputeManagementClient)
+            listVMs = VM.virtual_machines.list_all()
+
+            for i in listVMs:
+                string = str(i)
+                js = json.dumps(string)
+                return json.loads(js)
 
         except Exception as e:
             logging.error(msg=e)
